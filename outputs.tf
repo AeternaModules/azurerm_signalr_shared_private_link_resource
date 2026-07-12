97d82c7141cbbcaea5b0e448a143882c7609b393
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:25 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "signalr_shared_private_link_resources" {
-  description = "All signalr_shared_private_link_resource resources"
-  value       = azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources
-}
 output "signalr_shared_private_link_resources_name" {
-  description = "List of name values across all signalr_shared_private_link_resources"
-  value       = [for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : v.name]
+  description = "Map of name values across all signalr_shared_private_link_resources, keyed the same as var.signalr_shared_private_link_resources"
+  value       = { for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : k => v.name }
 }
 output "signalr_shared_private_link_resources_request_message" {
-  description = "List of request_message values across all signalr_shared_private_link_resources"
-  value       = [for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : v.request_message]
+  description = "Map of request_message values across all signalr_shared_private_link_resources, keyed the same as var.signalr_shared_private_link_resources"
+  value       = { for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : k => v.request_message }
 }
 output "signalr_shared_private_link_resources_signalr_service_id" {
-  description = "List of signalr_service_id values across all signalr_shared_private_link_resources"
-  value       = [for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : v.signalr_service_id]
+  description = "Map of signalr_service_id values across all signalr_shared_private_link_resources, keyed the same as var.signalr_shared_private_link_resources"
+  value       = { for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : k => v.signalr_service_id }
 }
 output "signalr_shared_private_link_resources_status" {
-  description = "List of status values across all signalr_shared_private_link_resources"
-  value       = [for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : v.status]
+  description = "Map of status values across all signalr_shared_private_link_resources, keyed the same as var.signalr_shared_private_link_resources"
+  value       = { for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : k => v.status }
 }
 output "signalr_shared_private_link_resources_sub_resource_name" {
-  description = "List of sub_resource_name values across all signalr_shared_private_link_resources"
-  value       = [for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : v.sub_resource_name]
+  description = "Map of sub_resource_name values across all signalr_shared_private_link_resources, keyed the same as var.signalr_shared_private_link_resources"
+  value       = { for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : k => v.sub_resource_name }
 }
 output "signalr_shared_private_link_resources_target_resource_id" {
-  description = "List of target_resource_id values across all signalr_shared_private_link_resources"
-  value       = [for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : v.target_resource_id]
+  description = "Map of target_resource_id values across all signalr_shared_private_link_resources, keyed the same as var.signalr_shared_private_link_resources"
+  value       = { for k, v in azurerm_signalr_shared_private_link_resource.signalr_shared_private_link_resources : k => v.target_resource_id }
 }
 
